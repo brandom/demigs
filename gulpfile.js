110ac55c45c8c2f475e859dcd3f870ed81dfd7c4
Brandon Van Noy <bvannoy@medfusionsvs.com>
2015-08-07 16:37:35 -0500

    chore(build): add revision to production build task

--- gulpfile.js
+++ gulpfile.js
@@ -11,32 +11,43 @@ gulp.task('js', function() {
   var vendor = gulp.src(bower())
     .pipe(plug.filter('*.js'))
     .pipe(plug.order(paths.vendorOrder));
-    
+
   var templates = gulp.src(paths.templates)
     .pipe(plug.angularTemplatecache('templates.js', {
       standalone: true
     }));
-    
+
   var js = gulp.src(paths.js)
     .pipe(plug.ngAnnotate());
-    
+
   return merge(vendor, templates, js)
     .pipe(plug.concat('demigs.js'))
     .pipe(gulp.dest(paths.public + 'js'));
 });
 
+gulp.task('prod-html-replace', ['js-prod'], function() {
+  var sources = require('./rev-manifest.json');
+  var js = sources['demigs.js'];
+  return gulp.src('public/index.html')
+    .pipe(plug.htmlReplace({'js': 'js/' + js}, {keepBlockTags: true}))
+    .pipe(gulp.dest('public/'));
+});
+
 gulp.task('js-prod', ['js'], function() {
   return gulp.src(paths.public + 'js/demigs.js')
     .pipe(plug.uglify())
-    .pipe(gulp.dest(paths.public + 'js'));
+    .pipe(plug.rev())
+    .pipe(gulp.dest(paths.public + 'js'))
+    .pipe(plug.rev.manifest())
+    .pipe(gulp.dest('./'));
 });
 
 gulp.task('css', function() {
   var vendor = gulp.src(bower())
     .pipe(plug.filter('*.css'));
-    
+
   var css = gulp.src(paths.css);
-  
+
   return merge(vendor, css)
     .pipe(plug.uglifycss())
     .pipe(plug.concat('demigs.css'))
@@ -46,19 +57,19 @@ gulp.task('css', function() {
 
 gulp.task('build', ['js', 'css']);
 
-gulp.task('prod', ['js-prod', 'css']);
+gulp.task('prod', ['prod-html-replace', 'css']);
 
 gulp.task('default', ['build', 'browser-sync', 'watch']);
 
 gulp.task('watch', function() {
   var js = [].concat(paths.js).concat(paths.templates);
   var css = [].concat(paths.css);
-  
+
   console.log('Watching all files');
 
   gulp.watch(css, ['css'])
     .on('change', logWatch);
-  
+
   gulp.watch(js, ['js'])
     .on('change', logWatch);
 
